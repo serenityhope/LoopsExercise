968bad2c0baaf541a9462a812515d04a0d543500
Serenity Button <serenitybutton@ituness-MacBook-Pro.local>
2020-10-26 10:37:28 -0500

    created loopsExercise.js file

---
+++ loopsExercise.js
@@ -0,0 +1,24 @@
+for (i=1; i <=7; i++){
+    console.log(i);
+}
+
+for (i=6; i<=6; i++){
+    console.log(i);
+}
+
+const wizards =[ 
+    "harry potter", 
+    "Hermione Granger",
+    "Ron Weasley"
+]
+
+for (name of wizards){
+    console.log(name);
+}
+
+let harryPotterMovies = 0
+
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
